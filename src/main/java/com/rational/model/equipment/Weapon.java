a38d823a270dde654a5f5c51fe59f5513d563f83
Andy <andyjwest@gmail.com>
2014-07-24 09:14:57 -0400

    fixing Weapon
    forgot a ; in weapon

--- src/main/java/com/rational/model/equipment/Weapon.java
+++ src/main/java/com/rational/model/equipment/Weapon.java
@@ -9,5 +9,37 @@ public class Weapon extends Equipment{
     private WeaponType weaponType;
     private int range;
     private Dice damageDice;
-    private int numberOfDice
+    private int numberOfDice;
+
+    public WeaponType getWeaponType() {
+        return weaponType;
+    }
+
+    public void setWeaponType(WeaponType weaponType) {
+        this.weaponType = weaponType;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public Dice getDamageDice() {
+        return damageDice;
+    }
+
+    public void setDamageDice(Dice damageDice) {
+        this.damageDice = damageDice;
+    }
+
+    public int getNumberOfDice() {
+        return numberOfDice;
+    }
+
+    public void setNumberOfDice(int numberOfDice) {
+        this.numberOfDice = numberOfDice;
+    }
 }
