0b0c65bc3f2f1b29dda5cdcb4e5c5a7b4b866abb
Peter Varner-Howland <pvarnerhowland@atd-us.com>
2014-07-31 15:51:52 -0400

    user is now prompted to select a number of skill proficiencies equal to the number of skills allowed by the selected class. Added jquery-ui to allow the creation of modals. implemented a language selection system. user can select a number of languages equal to their intelligence modifier.

--- src/main/webapp/resources/js/require-config.js
+++ src/main/webapp/resources/js/require-config.js
@@ -19,7 +19,8 @@ require.config({
         //Models
         CharacterModel: 'Model/CharacterModel',
         //Libs
-        jquery: 'lib/jquery-1.8.3',
+        jquery: 'lib/jquery',
+        jqueryUi: 'lib/jquery-ui',
         underscore: 'lib/underscore',
         backbone: 'lib/backbone',
         marionette: 'lib/marionette'
