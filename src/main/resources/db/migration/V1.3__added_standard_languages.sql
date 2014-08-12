0acbe071ea7985cfa1acd4b8ffdcd68a820bbe35
Andy <andyjwest@gmail.com>
2014-08-12 10:48:14 -0400

    Adding some races

---
+++ src/main/resources/db/migration/V1.3__added_standard_languages.sql
@@ -0,0 +1,8 @@
+INSERT INTO `charactersheet`.`languages` (`id`, `name`) VALUES ('1', 'Common');
+INSERT INTO `charactersheet`.`languages` (`id`, `name`) VALUES ('2', 'Dwarvish');
+INSERT INTO `charactersheet`.`languages` (`id`, `name`) VALUES ('3', 'Elvish');
+INSERT INTO `charactersheet`.`languages` (`id`, `name`) VALUES ('4', 'Giant');
+INSERT INTO `charactersheet`.`languages` (`id`, `name`) VALUES ('5', 'Gnomish');
+INSERT INTO `charactersheet`.`languages` (`id`, `name`) VALUES ('6', 'Goblin');
+INSERT INTO `charactersheet`.`languages` (`id`, `name`) VALUES ('7', 'Halfling');
+INSERT INTO `charactersheet`.`languages` (`id`, `name`) VALUES ('8', 'Orc');
