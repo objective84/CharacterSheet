8d2bfb9f0c5f95e8eb4783d89284f9d6b1a3afc1
Andy <andyjwest@gmail.com>
2014-08-21 10:33:32 -0400

    Missed these??

--- src/main/resources/db/migration/V1.21__create_character_advancement_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `charactersheet`.`character_advancement` (
-  `id_character_advancement` INT NOT NULL,
-  `total_level` INT NULL,
-  `proficiency_bonus` INT NULL,
-  `experience_points` INT NULL,
-  PRIMARY KEY (`id_character_advancement`));
