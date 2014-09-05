0e28d71c051721135e924206a19c957db087fd98
Peter Varner-Howland <pvarnerhowland@atd-us.com>
2014-09-05 12:52:49 -0400

    Fixed flyway error

--- src/main/resources/db/migration/V1.39__adding_spells_C.sql
+++ src/main/resources/db/migration/V1.39__adding_spells_C.sql
@@ -732,13 +732,13 @@ VALUES ('76', #id
         '1 action', #castingTime
         'Until the spell ends, you control any freestanding water inside an area you choose that is a cube up to 100 feet on a side. You can choose from any of the following effects when you cast this spell. As an action on your turn, you can repeat the same effect or choose a different one. /n/bFlood./b You cause the water level of all standing water in the area to rise by as much as 20 feet. If the area includes a shore, the flooding water spills over onto dry land. /nIf you choose an area in a large body of water, you instead create a 20-foot tall wave that travels from one side of the area to the other and then crashes down. Any Huge or smaller vehicles in the wave’s path are carried with it to the other side. Any Huge or smaller vehicles struck by the wave have a 25 percent chance of capsizing. /nThe water level remains elevated until the spell ends or you choose a different effect. If this effect produced a wave, the wave repeats on the start of your next turn while the flood effect lasts. /n/bPart Water./b You cause water in the area to move apart and create a trench. The trench extends across the spell’s area, and the separated water forms a wall to either side. The trench remains until the spell ends or you choose a different effect. The water then slowly fills in the trench over the course of the next round until the normal water level is restored. /n/bRedirect Flow./b You cause flowing water in the area to move in a direction you choose, even if the water has toflow over obstacles, up walls, or in other unlikely directions. The water in the area moves as you direct it, but once it moves beyond the spell’s area, it resumes its flow based on the terrain conditions. The water continues to move in the direction you chose until the spell ends or you choose a different effect. /n/bWhirlpool./b This effect requires a body of water at least 50 feet square and 25 feet deep. You cause a whirlpool to form in the center of the area. The whirlpool forms a vortex that is 5 feet w ide at the base, up to 50 feet w ide at the top, and 25 feet tall. Any creature or object in the water and within 25 feet of the vortex is pulled 10 feet toward it. A creature can swim away from the vortex by making a Strength (Athletics) check against your spell save DC. /nWhen a creature enters the vortex for the first time on a turn or starts its turn there, it must make a Strength saving throw. On a failed save, the creature takes 2d8 bludgeoning damage and is caught in the vortex until the spell ends. On a successful save, the creature takes half damage, and isn’t caught in the vortex. A creature caught in the vortex can use its action to try to swim away from the vortex as described above, but has disadvantage on the Strength (Athletics) check to do so. /nThe first time each turn that an object enters the vortex, the object takes 2d8 bludgeoning damage; this damage occurs each round it remains in the vortex.', #description
         '4', #level
-        '', #damageDiceAmount
-        '', #damageDice ID
-        '', #damageType ID
+        null, #damageDiceAmount
+        null, #damageDice ID
+        null, #damageType ID
         '', #savingThrow
         '', #condition
-        '', #requiresVerbalComponent
-        '0', #requiresSomaticComponent
+        '1', #requiresVerbalComponent
+        '1', #requiresSomaticComponent
         'drop of water and a pinch of dust', #materialComponent
         'Concentration, up to 10 minutes', #duration
         'freestanding water in a cube up to 100 feet', #target
